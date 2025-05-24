9d9f0f3fa1a85435d651f40b220e9b0b4cf13795
Rahulreddy2003 <122749010+Rahulreddy2003@users.noreply.github.com>
2025-05-24 08:50:21 +0530

    Main.java

---
+++ src/main/java/ums/Main.java
@@ -0,0 +1,8 @@
+package ums;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server logic should go here.");
+        // Start any needed server, database connection, etc.
+    }
+}
